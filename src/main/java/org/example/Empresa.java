6edb8bf92f90c955b17f0234cfef6ed991f75946
mjosepc <mjose.powell@gmail.com>
2023-11-02 10:17:59 -0300

    clase empresa

--- src/main/java/org/example/Empresa.java
+++ src/main/java/org/example/Empresa.java
@@ -1,13 +1,22 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empresa {
 
 	private String nombreComercial;
 	private String direccion;
+	List<Sucursal> sucursales;
 
-	public String getNombreComercial() {
-		return this.nombreComercial;
+	public Empresa(String nombreComercial, String direccion) {
+		this.nombreComercial = nombreComercial;
+		this.direccion = direccion;
+		this.sucursales = new ArrayList<>();
 	}
 
+	public String getNombreComercial() {
+			return this.nombreComercial;
+		}
 	public void setNombreComercial(String nombreComercial) {
 		this.nombreComercial = nombreComercial;
 	}
